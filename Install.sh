32db1a3773b6c058d31d1f612eda2a29341f4f9a
Condor2026 <268162817+Condor2026@users.noreply.github.com>
2026-03-22 00:26:26 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,7 +7,7 @@ pkg install python git -y
 pip install requests phonenumbers whois dnspython
 
 echo "Clonando repositorio..."
-git clone https://github.com/tu-usuario/LYRA.git
-cd LYRA
+git clone https://github.com/Condor2026/Lyra_.git
+cd Lyra_
 
 echo "Instalación completada. Ejecuta: python lyra.py"
